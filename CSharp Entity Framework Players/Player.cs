9c0be2590deb0d7429f1c7cc8ed660fea8c23c78
Pedra96 <darkraidialga@yahoo.it>
2022-12-22 15:35:54 +0100

    Add project files.

---
+++ CSharp Entity Framework Players/Player.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Entity_Framework_Players {
+
+    [Table("giocatori")]
+    [Index(nameof(GiocatoreID), IsUnique = true)]
+    public class Student {
+
+        [Key]
+        [Column("primary_Key")]
+        [Required]
+        public int GiocatoreID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [Column("punteggio")]
+        public float Punteggio { get; set; }
+        [Column("partite_giocate")]
+        public int PartiteGiocate { get; set; }
+        [Column("partite_vinte")]
+        public int PartiteVinte { get; set; }
+    }
+}
---
+++ CSharp Entity Framework Players/TeamContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Entity_Framework_Players {
+    public class TeamContext:DbContext {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=TeamDB;" +
+            "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
